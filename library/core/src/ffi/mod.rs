34b3c7efb7eaa39b235ac8c736b9cb5a23063c48
Yonggang Luo <luoyonggang@gmail.com>
2023-05-08 17:13:18 +0800

    stablize it

--- library/core/src/ffi/mod.rs
+++ library/core/src/ffi/mod.rs
@@ -55,7 +55,6 @@ macro_rules! type_alias {
         type_alias_no_nz! { $Docfile, $Alias = $Real; $( $Cfg )* }
 
         #[doc = concat!("Type alias for `NonZero` version of [`", stringify!($Alias), "`]")]
-        #[unstable(feature = "raw_os_nonzero", issue = "82363")]
         $( $Cfg )*
         pub type $NZAlias = $NZReal;
     }
